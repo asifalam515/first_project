0be1ff4feb8036f26a94e0f56c4ecf555d2edc8a
asifalam515 <asibulalam515@gmail.com>
2025-02-13 16:56:26 +0600

    set up done

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,15 @@
-import { Request, Response } from "express";
-
+import { Application, Request, Response } from "express";
+import cors from "cors";
 const express = require("express");
-const app = express();
+const app: Application = express();
 const port = 3000;
-
+app.use(express.json());
+app.use(cors());
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+  res.send("hello world");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`);
+// });
+export default app;
