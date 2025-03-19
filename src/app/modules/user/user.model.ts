20ad77a0b3084accba7a97ae44f474b7bf0f49f0
asifalam515 <asibulalam515@gmail.com>
2025-03-19 12:11:27 +0600

    time stamp added

--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -1,14 +1,25 @@
 import { Schema } from "mongoose";
 import { TUser } from "./user.interface";
 
-const userSchema = new Schema<TUser>({
-  id: { type: String, required: true },
-  status: { type: String },
-  password: { type: String, required: true },
-  needsPassword: { type: Boolean, default: true },
-  role: {
-    type: String,
-    enum: ["admin", "student", "faculty"],
-    default: "student",
+const userSchema = new Schema<TUser>(
+  {
+    id: { type: String, required: true },
+
+    password: { type: String, required: true },
+    needsPassword: { type: Boolean, default: true },
+    role: {
+      type: String,
+      enum: ["student", "faculty", "admin"],
+      default: "student",
+    },
+    status: {
+      type: String,
+      enum: ["in-progress", "blocked"],
+    },
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
   },
-});
+  { timestamps: true }
+);
